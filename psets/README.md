6fd2e4bc1d6b652bd43cb39f6d7af5764646a64f
DavidHdezU <Dhdezu@ciencias.unam.mx>
2022-03-08 00:57:45 -0600

    pset2 completed...

---
+++ psets/README.md
@@ -0,0 +1,5 @@
+# Práctica02
+## Integrantes
+
+- David Hernández Uriostegui
+- Joshua Said Montaño Pérez
--- psets/pset2.rkt
+++ psets/pset2.rkt
@@ -61,7 +61,75 @@
     [else (auxCuenta (cdr curr_lista) lista_org elem)]))
 
 
+;; función tipo punto
+(define-type Punto
+[punto (x number?) (y number?)])
 
+;; Funciones auxliares para obtener la coordenada en x y en y
+
+;; getX: Punto -> number
+(define (getX p)
+  (type-case Punto p
+    [punto (x y) x]))
+
+;; getX: Punto -> number
+(define (getY p)
+  (type-case Punto p
+    [punto (x y) y]))
+
+
+
+;; punto-medio: Punto Punto -> number
+(define (punto-medio p q)
+  (if (or (not (Punto? p)) (not (Punto? q)))
+      (error "Alguno de los dos (p, q) no es puntp")
+      (punto (/ (+ (getX p) (getX q)) 2) (/ (+ (getY p) (getY q)) 2))))
+
+;; distancia: Punto Punto -> number
+(define (distancia p q)
+  (if (or (not (Punto? p)) (not (Punto? q)))
+      (error "Alguno de los dos (p, q) no es punto")
+      (sqrt (+ (* (- (getX q) (getX p)) (- (getX q) (getX p))) (* (- (getY q) (getY p)) (- (getY q) (getY p)))))))
+
+
+;; Definición del tipo abstracto de datos Figura
+(define-type Figura
+[Circulo (centro Punto?)
+         (radio number?)]
+[Triangulo (punto1 Punto?)
+           (punto2 Punto?)
+           (punto3 Punto?)]
+[Cuadrado (punto Punto?)
+          (lado number?)]
+[Rectangulo (punto Punto?)
+            (largo number?)
+            (altura number?)])
+
+
+;; perimetro: Figura -> number
+(define (perimetro fig)
+  (if (not (Figura? fig))
+      (error "Esto no es una figura")
+      (type-case Figura fig
+        [Circulo (c r) (* 2 pi r)]
+        [Triangulo (a b c) (+ (+ (distancia a b) (distancia b c)) (distancia c a))]
+        [Cuadrado (p l) (* 4 l)]
+        [Rectangulo (p l a) (+ (* 2 l) (* 2 a))])))
+
+;; perimetro: Figura -> number
+(define (area fig)
+  (if (not (Figura? fig))
+      (error "Esto no es una figura")
+      (type-case Figura fig
+        [Circulo (c r) (* pi (* r r))]
+        [Triangulo (a b c) (area_triangulo a b c)]
+        [Cuadrado (p l) (* l l)]
+        [Rectangulo (p l a) (+ (* 2 l) (* 2 a))])))
+
+;; Función auxiliar para calcular área de un triángulo dado 3 puntos
+;; area_triangulo; Punto -> Punto -> Punto -> number
+(define (area_triangulo a b c)
+  (exact->inexact abs((/ (+ (* (getX a) (- (getY b) (getY c))) (* (getX b) (- (getY c) (getY a))) (* (getX c) (- (getY a) (getY b)))) 2))))
 
 
 
